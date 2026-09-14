03c8d5d5c1caebfae12045713ef95e2237371dde
thilva <thi.ilvaa@gmail.com>
2026-09-13 21:54:07 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from google import genai
+from google.genai import types
 
 # Configuração da página do Streamlit
 st.set_page_config(page_title="Resultados Desportivos Ao Vivo", page_icon="⚽", layout="wide")
